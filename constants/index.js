4672ef8bee74459ac7471a141bc1ffa31e184a7f
LeandroMedvedev <medvedevleandro@outlook.com>
2024-11-06 16:55:25 -0300

    feat: task fifteen completed

--- constants/index.js
+++ constants/index.js
@@ -54,3 +54,50 @@ export const MOVIES = [
     startTime: '19:15',
   },
 ];
+
+export const QUESTIONS = [
+  {
+    question: 'Qual é o operador usado para atribuição em JavaScript?',
+    options: ['=', '==', '===', ':'],
+    answer: '=',
+  },
+  {
+    question:
+      'Qual das opções abaixo representa um tipo de dado em JavaScript?',
+    options: ['String', 'Character', 'Text', 'Word'],
+    answer: 'String',
+  },
+  {
+    question: 'Como você declara uma variável constante em JavaScript?',
+    options: ['const', 'let', 'var', 'constant'],
+    answer: 'const',
+  },
+  {
+    question:
+      'Qual método é usado para adicionar elementos ao final de um array em JavaScript?',
+    options: ['push', 'pop', 'shift', 'unshift'],
+    answer: 'push',
+  },
+  {
+    question:
+      'Qual estrutura de repetição é usada para iterar sobre propriedades de um objeto em JavaScript?',
+    options: ['for', 'for...of', 'for...in', 'foreach'],
+    answer: 'for...in',
+  },
+  {
+    question:
+      'Qual função converte uma string para um número inteiro em JavaScript?',
+    options: ['parseInt', 'Number', 'toString', 'parseFloat'],
+    answer: 'parseInt',
+  },
+  {
+    question: "O que 'NaN' representa em JavaScript?",
+    options: [
+      'Número negativo',
+      'Número muito grande',
+      'Valor inválido',
+      'Não é um número',
+    ],
+    answer: 'Não é um número',
+  },
+];
--- tasks/task-15/index.jsx
+++ tasks/task-15/index.jsx
@@ -1,17 +1,110 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { colors, globalStyles } from '../../styles/globalStyles';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+import { QUESTIONS } from '../../constants';
+import { colors } from '../../styles/globalStyles';
 
 export default function Task15() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [isQuizFinished, setIsQuizFinished] = useState(false);
+
+  const handleAnswer = (option) => {
+    if (option === QUESTIONS[currentQuestion].answer) {
+      setScore(score + 1);
+    }
+
+    if (currentQuestion < QUESTIONS.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      setIsQuizFinished(true);
+    }
+  };
+
+  const restartQuiz = () => {
+    setCurrentQuestion(0);
+    setScore(0);
+    setIsQuizFinished(false);
+  };
+
   return (
-    <View style={globalStyles.container}>
-      <Text style={styles.text}>Tarefa 15</Text>
+    <View style={styles.container}>
+      {isQuizFinished ? (
+        <View style={styles.scoreContainer}>
+          <Text style={styles.scoreText}>
+            Pontuação Final: {score}/{QUESTIONS.length}
+          </Text>
+          <TouchableOpacity style={styles.button} onPress={restartQuiz}>
+            <Text style={styles.buttonText}>Reiniciar Quiz</Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View style={styles.questionContainer}>
+          <Text style={styles.questionText}>
+            {QUESTIONS[currentQuestion].question}
+          </Text>
+          {QUESTIONS[currentQuestion].options.map((option, index) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.optionButton}
+              onPress={() => handleAnswer(option)}
+            >
+              <Text style={styles.optionText}>{option}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 28,
+  container: {
+    margin: 'auto',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: colors.lightGrey,
+  },
+  questionContainer: {
+    width: '100%',
+  },
+  questionText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  optionButton: {
+    backgroundColor: colors.ciano,
+    padding: 15,
+    marginVertical: 5,
+    borderRadius: 5,
+    width: '100%',
+    alignItems: 'center',
+  },
+  optionText: {
     color: colors.white,
+    fontSize: 18,
+  },
+  scoreContainer: {
+    alignItems: 'center',
+  },
+  scoreText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: colors.githubGreen,
+    padding: 15,
+    borderRadius: 5,
+    width: '100%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
